e8bca56c140639a475cd6569e4480c7653896a20
Angelina1379 <lina.pozolotina.03@mail.ru>
2026-07-17 10:54:16 +0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -350,3 +350,46 @@ initReveal();
 loadReviews();
   
 });
+
+window.openReviewModal=function(review){
+
+    const modal=document.getElementById("reviewModal");
+
+    document.getElementById(
+        "modalReviewName"
+    ).textContent=review.name;
+
+
+    document.getElementById(
+        "modalReviewStars"
+    ).innerHTML=
+    "★".repeat(review.rating)+
+    "☆".repeat(5-review.rating);
+
+
+    document.getElementById(
+        "modalReviewDate"
+    ).textContent=
+    new Date(review.date)
+    .toLocaleDateString("ru-RU");
+
+
+    document.getElementById(
+        "modalReviewText"
+    ).textContent=
+    review.message;
+
+
+    modal.classList.add("open");
+
+}
+
+
+
+window.closeReviewModal=function(){
+
+    document
+    .getElementById("reviewModal")
+    .classList.remove("open");
+
+}
